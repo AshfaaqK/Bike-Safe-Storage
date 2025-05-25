d800904ae16f127c2e038a01ad75da722858ad1e
AshfaaqK <ashfaaqkhodabux@gmail.com>
2025-05-25 21:02:03 +0100

    feat: change collapsable navbar to offcanvas for more a responsive design

--- main.py
+++ main.py
@@ -101,5 +101,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/register')
+def register():
+
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
